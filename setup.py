27411202d7fbfed1925b10196ff3ee026c9fe2ef
hypergravity <bozhang123>
2018-12-12 18:25:27 +0800

    updated setup.py to upload to PYPI

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-from distutils.core import setup
+import setuptools
 
-if __name__ == '__main__':
-    setup(
-        name='regli',
-        version='0.0.2',
-        author='Bo Zhang',
-        author_email='bzhang@mpia.de',
-        # py_modules=['bopy','spec','core'],
-        description='REgular Grid Linear Interpolator.',  # short description
-        license='MIT',
-        # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
-        url='http://github.com/hypergravity/regli',
-        classifiers=[
-            "Development Status :: 6 - Mature",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3.6",
-            "Topic :: Scientific/Engineering :: Astronomy",
-            "Topic :: Scientific/Engineering :: Physics"],
-        package_dir={'regli': 'regli'},
-        packages=['regli', ],
-        #package_data={"ruby": ["data/*", "script/*"]},
-        include_package_data=True,
-        requires=['numpy', 'scipy', 'matplotlib']
-    )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='regli',
+    version='0.0.2',
+    author='Bo Zhang',
+    author_email='bozhang@nao.cas.cn',
+    description='REgular Grid Linear Interpolator.',  # short description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/hypergravity/regli',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics"],
+    package_dir={'regli': 'regli'},
+    include_package_data=True,
+    requires=['numpy', 'scipy']
+)
