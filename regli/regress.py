4fd17dd81c30b976651f6ef1a0516b47966d61aa
hypergravity <bozhang123>
2018-12-12 22:20:58 +0800

    fixed a bug

--- regli/regress.py
+++ regli/regress.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 def costfun(x, r, v_obs, v_err):
@@ -5,5 +6,9 @@ def costfun(x, r, v_obs, v_err):
 
 
 def default_lnpost(x, r, obs, obs_err):
-    return 0.5 * ((r(x) - obs) / (obs_err + r.me))**2.
+    lnpost = - 0.5 * np.sum(((r(x) - obs) / (obs_err + r.me))**2.)
+    if np.isfinite(lnpost):
+        return lnpost
+    else:
+        return -np.inf
 
