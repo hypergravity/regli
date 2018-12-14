cee9395953287c4f23e56be19e3c2f98d8fab966
hypergravity <bozhang123>
2018-12-14 22:10:01 +0800

    updated to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='regli',
-    version='0.0.4',
+    version='0.0.5',
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description='REgular Grid Linear Interpolator.',  # short description
@@ -14,15 +14,14 @@ setuptools.setup(
     url='http://github.com/hypergravity/regli',
     packages=setuptools.find_packages(),
     license='MIT',
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Topic :: Scientific/Engineering :: Physics"],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Astronomy"],
     package_dir={'regli': 'regli'},
     include_package_data=True,
-    requires=['numpy', 'scipy']
+    requires=['numpy', 'scipy', 'emcee']
 )
