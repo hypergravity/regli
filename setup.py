3408201af9476f13bfe84c0b0cb22685bfed0abb
Bo Zhang <bozhang@nao.cas.cn>
2019-08-11 18:14:46 +0800

    version 2019.0811.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='regli',
-    version='0.2.1',
+    version='2019.0811.0',
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description='REgular Grid Linear Interpolator.',  # short description
