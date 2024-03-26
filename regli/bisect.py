8da4caf3c9dd7ab1b6dc74d91978a7c7e68be897
BO ZHANG <bozhang@nao.cas.cn>
2024-03-26 17:44:11 +0800

    add bisect.find_neighboring_indices()

---
+++ regli/bisect.py
@@ -0,0 +1,62 @@
+import bisect
+from typing import Iterable
+
+
+def find_neighboring_indices(seq: Iterable, a: int) -> tuple[int, int]:
+    r"""Find neighboring indices in a sequence for value ``a`` .
+
+    Prefer left interval if on grid points.
+
+    Parameters
+    ----------
+    seq: Iterable
+        Sequence data.
+    a: int
+        Target value.
+
+    Returns
+    -------
+    tuple[int, int]
+        The left and right indices.
+    """
+    l = len(seq)
+    if seq[0] < a < seq[-1]:
+        i_l = bisect.bisect_left(seq, a)
+        i_r = bisect.bisect_right(seq, a)
+        if i_l < i_r:
+            # exactly on grid points
+            return i_l - 1, i_r - 1
+        else:
+            # between grid points
+            return i_l - 1, i_r
+    # left most
+    if a == seq[0]:
+        return 0, 1
+    # right most
+    if a == seq[-1]:
+        return l - 2, l - 1
+    # out of bounds
+    return -1, -1
+
+
+def f(a):
+    seq = [1, 2, 3, 4]
+    print(f"{a} \t", find_neighboring_indices(seq, a))
+
+
+def f1(a):
+    seq = [1, 2, 3, 4]
+    print(f"{a} \t", bisect.bisect_left(seq, a), bisect.bisect_right(seq, a))
+
+
+if __name__ == "__main__":
+    f(0.999)
+    f(1)
+    f(1.5)
+    f(2)
+    f(2.5)
+    f(3)
+    f(3.5)
+    f(4)
+    f(4.5)
+    f1(5)
